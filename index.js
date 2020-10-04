63c22ed558864f9adcd88007c18cb3628a6d4d46
brammie15 <brammieverhulst@gmail.com>
2020-10-04 16:37:15 +0200

    Add lots of new things
    small things like integrated console and big brain

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const request = require('request');
-const cheerio = require('cheerio');
+//const cheerio = require('cheerio');
+const readline = require('readline');
+const rl = readline.createInterface({input : process.stdin,output : process.stdout});
 
+rl.on("line",(userInput)=>{
+    var debugChannel = client.channels.cache.get('762229279756779550');
+    debugChannel.send(userInput);
+});
+var date_ob = new Date();
+var interval;
 var Count = 0;
-const { prefix, token } = require('./botconfig.json');
+const { prefix } = require('./botconfig.json');
+const { userInfo } = require('os');
+//const { RSA_X931_PADDING } = require('constants');
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -18,23 +30,26 @@ client.on('message', message => {
             .setAuthor("Useless Name")
             .setDescription("Useless Help")
             .addFields(
-              { name: "!tmauf", value: "tell me an useless fact"},
-              { name: "!tmtmofut", value: "tell the most useless fact of today"},
+            { name: "!tmauf", value: "tell me an useless fact"},
+            { name: "!tmtmofut", value: "tell the most useless fact of today"},
              // { name: "!ihrttpisf", value: "i have record that this person is simping for"},
-              { name: "!ping", value: "i don't know what this does tbh"},
-              { name: "!gmarn", value: "give me a random number + max"},
-              { name: "!hmphgauf", value: "how many people have gotten an useless fact"},
-              { name: "!htcueodyfhdotey", value: "has the conceivable universe exploded or died of heat death or thermal expansion yet?"},
-              { name: "!nou", value: "no u, the perfect tool for uno"},
+            { name: "!ping", value: "i don't know what this does tbh"},
+            { name: "!pong", value: "don't know about this one either tbh"},
+              //{ name: "!gmarn", value: "give me a random number + max"},
+            { name: "!hmphgauf", value: "how many people have gotten an useless fact"},
+            { name: "!htcueodyfhdotey", value: "has the conceivable universe exploded or died of heat death or thermal expansion yet?"},
+            //{ name: "!nou", value: "no u, the perfect tool for uno"},
             )
             .setTimestamp()
             .setFooter("useless footer");
         
 
-        message.channel.send(Help)
+        message.channel.send(Help);
 
     }else if(command === `ping`){
         return message.channel.send("pong");
+    }else if(command ==='pong'){
+        return message.channel.send("ping");
     }else if(command === `tmauf`){
         request('https://uselessfacts.jsph.pl/random.json/?language=en', (error,respone,body) =>{
             if(!error && respone.statusCode == 200){
@@ -53,11 +68,9 @@ client.on('message', message => {
         
         client.user.setPresence({status:"invisible"});
     }else if(command ===`nou`){
-        message.channel.send('⠐⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠂\n⠄⠄⣰⣾⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣆⠄⠄\n⠄⠄⣿⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣇⠔⠈⣿⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦⡀⠹⠄⠄\n⠄⠄⣿⣿⠃⠄⢠⣾⣿⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⣿⣿⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⡟⠄⠄\n⠄⠄⣿⡿⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿⣿⠃⠄⠄\n⠄⠄⡿⠁⣼⣿⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢠⠄⠄\n⠄⠄⠃⢰⣿⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⠄⠄\n⠄⠄⢀⣿⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⣿⠄⠄\n⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⣿⠗⠄⠄⣿⣿⠄⠄\n⠄⠄⣆⠈⠻⢿⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣿⣇⠠⠺⣷⣿⣿⠄⠄\n⠄⠄⣿⣿⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿⣿⠄⠄\n⠄⠄⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⡿⠟⠄⠄\n⠠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄.')
+        //message.channel.send('⠐⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠂\n⠄⠄⣰⣾⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣆⠄⠄\n⠄⠄⣿⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣇⠔⠈⣿⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦⡀⠹⠄⠄\n⠄⠄⣿⣿⠃⠄⢠⣾⣿⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⣿⣿⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⡟⠄⠄\n⠄⠄⣿⡿⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿⣿⠃⠄⠄\n⠄⠄⡿⠁⣼⣿⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢠⠄⠄\n⠄⠄⠃⢰⣿⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⠄⠄\n⠄⠄⢀⣿⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⣿⠄⠄\n⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿⣿⣿⠄⠄\n⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⠄⠄\n⠄⠄⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⣿⠗⠄⠄⣿⣿⠄⠄\n⠄⠄⣆⠈⠻⢿⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣿⣇⠠⠺⣷⣿⣿⠄⠄\n⠄⠄⣿⣿⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿⣿⠄⠄\n⠄⠄⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⡿⠟⠄⠄\n⠠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄.')
     }else if(command === `hmphgauf`){
-        message.channel.send(Count +" people have recieved an useless fact.")
-    }else if(command === `hmphgauf`){
-
+        message.channel.send(Count +" people have recieved an useless fact.");
     }else if(command === `tmtmufot`){
         request('https://uselessfacts.jsph.pl/today.json?language=en', (error,respone,body) =>{
             if(!error && respone.statusCode == 200){
@@ -65,60 +78,52 @@ client.on('message', message => {
                 var uselessFactToday = imported['text'];
                 Count ++;
                 message.channel.send("here is the most useless fact of today " + uselessFactToday)
-                
-                
             }
         });
-
     }else if(command === 'gmarn'){
         if(!args[0]){
-           return message.channel.send("i am gonna need a maxinum number.")
+            return message.channel.send("i am gonna need a maxinum number.")
         }
         message.channel.send(Math.ceil(Math.random()*  parseFloat(args[0])));
-    }
-    
-    
-    /*else if(command === `ihrttpisf`){
-
-        //message.channel.send("this person talked to me: "+message.member.user);
-        const user = message.author;
-
-        if(!args.length){
-            return message.channel.send(`u r soo stupid u need to provide arguments, ${message.author}!`)
-        }
-	    //message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-    
-    }*/else if(command === `test`){
+    }else if(command === `test`){
         if(message.member.user.tag == "brammie15#3455"){
-            channel = client.channels.cache.get('756815503925837933');
-            message.channel.send("bot is struggeling to start...");
-            var interval = setInterval (function () {
-                request('https://uselessfacts.jsph.pl/random.json/?language=en', (error,respone,body) =>{
-                    if(!error && respone.statusCode == 200){
-                    var imported = JSON.parse(body);
-                    var uselessFact = imported['text'];
-                    Count ++;
-                    message.channel.send("here is ur useless fact: " + uselessFact)
-                
-            }
-        });
-            }, 900000);
+            channel = client.channels.cache.get('762229279756779550');
+            channel.send("bot is struggeling to start...");
+            interval = setInterval (function () {
+                var hour = date_ob.getHours();
+                hour = (hour < 10 ? "0" : "") + hour;
+                if(hour < 22 && hour > 8){
+                    request('https://uselessfacts.jsph.pl/random.json/?language=en', (error,respone,body) =>{
+                        if(!error && respone.statusCode == 200){
+                            var imported = JSON.parse(body);
+                            var uselessFact = imported['text'];
+                            Count ++;
+                            channel.send("here is ur useless fact: " + uselessFact)
+                        }
+                    });
+                }
+            }, 3600000);
         }else{
             message.channel.send("u dont hav perms");
         }
-
-       // message.channel.send(`First argument: ${args[0]}`);
     }
     else if(command === "htcueodyfhdotey"){
-       
+
         message.channel.send("i dont know");
         
+    }else if(command === 'stopit'){
+        if(message.member.user.tag == "brammie15#3455"){
+            clearInterval(interval);
+        }else{
+            message.channel.send("u dont hav perms")
+        }
     }
 	console.log(message.content);
 });
-
-
+function testing(helo){
+    channel = client.channels.cache.get('756815503925837933');
+    channel.send(helo);
+}
 client.once('ready', () => {
 	console.log('Ready!');
 });
